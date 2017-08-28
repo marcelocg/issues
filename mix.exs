e873b6ba567a20ef94287fb2112f87a7afcc7e0e
Marcelo Gonçalves <marcelocg@gmail.com>
2017-08-28 19:09:11 +0000

    Add automatic documentation

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule Issues.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      name: "Issues",
+      source_url: "https://github.com/marcelocg/issues",
       deps: deps()
     ]
   end
@@ -23,7 +25,10 @@ defmodule Issues.Mixfile do
   defp deps do
     [
       {:httpoison, "~> 0.13.0"},
-      {:poison, "~> 3.1.0"},
+      {:poison,    "~> 3.1.0"},
+      {:ex_doc,    "~> 0.16.3"},
+      {:earmark,   "~> 1.2.3", override: true},
+      
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
